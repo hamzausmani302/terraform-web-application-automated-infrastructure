9c713cb04a929a3e4c5b85fd510aac566e5b871d
hamzausmani302 <hamzausmani021@gmail.com>
2023-05-29 22:11:07 +0500

    saving progress

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "web_server_ami" {
     type = string
     default = "ami-07c433cc0a3274562"
 }
+
+variable "subscriber_emails" {
+  type    = list(string)
+  default = ["hamzausmani021@gmail.com"]
+}
+
+variable "subscriber_mobile" {
+    type=   list(string)
+    default =   ["+923322411710"]
+}
